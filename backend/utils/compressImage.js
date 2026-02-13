f8a370f2055927fe06970357abcf576306b4ba38
alex <alexanderjannespia0127@gmail.com>
2026-02-13 17:27:12 +0800

    fix (backend): add support for heic photos

--- backend/utils/compressImage.js
+++ backend/utils/compressImage.js
@@ -1,13 +1,20 @@
+import heicConvert from "heic-convert";
 import sharp from "sharp";
 
 export const getCompressedImageBuffer = async (image) => { 
     
-    const buffer = await sharp(image)
+    const convertBuffer = await heicConvert({
+        buffer: inputHeicBuffer,
+        format: "JPEG",
+        quality: 1
+    });
+
+    const final = await sharp(convertBuffer)
         .resize({ width: 256 })
         .jpeg({ quality: 40 })
         .toBuffer();
 
     console.log(`Image Compressed`);
 
-    return buffer;
+    return final;
 }
