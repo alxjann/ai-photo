61d1fc55e3d90edb6adcbb4872b441db4b6500a4
alex <alexanderjannespia0127@gmail.com>
2026-02-13 20:22:48 +0800

    fix (backend): fix heic format not supported

--- backend/utils/compressImage.js
+++ backend/utils/compressImage.js
@@ -1,20 +1,22 @@
 import heicConvert from "heic-convert";
 import sharp from "sharp";
 
-export const getCompressedImageBuffer = async (image) => { 
-    
-    const convertBuffer = await heicConvert({
-        buffer: inputHeicBuffer,
-        format: "JPEG",
-        quality: 1
+export const getCompressedImageBuffer = async (imageBuffer) => {
+  let bufferToProcess = imageBuffer;
+
+  const isHeic = imageBuffer.slice(4, 12).toString().includes("ftypheic");
+
+  if (isHeic) {
+    bufferToProcess = await heicConvert({
+      buffer: imageBuffer,
+      format: "JPEG",
+      quality: 1
     });
+  }
 
-    const final = await sharp(convertBuffer)
-        .resize({ width: 256 })
-        .jpeg({ quality: 40 })
-        .toBuffer();
-
-    console.log(`Image Compressed`);
-
-    return final;
-}
+  const final = await sharp(bufferToProcess)
+    .resize({ width: 256 })
+    .jpeg({ quality: 40 })
+    .toBuffer();
+  return final;
+};
